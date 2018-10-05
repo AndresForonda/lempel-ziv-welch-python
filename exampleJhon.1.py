3dc5aeb0499871710392c7ead286defa2408e3cd
Jhon Jairo Sanchez Benavides <j.jairosanchez.b@gmail.com>
2018-10-05 08:00:41 -0500

    bug numeros binario cambiados. compresion arreglada. Libros en latin1

--- exampleJhon.1.py
+++ exampleJhon.1.py
@@ -11,70 +11,134 @@ from collections import deque
 
 #arguments management
 parser = argparse.ArgumentParser("compresor y descompresor lempel ziv")
-parser.add_argument(
-    '-a',
-    '--action',
-    required=True,
-    help='Comprimir(c) o Descomprimir(u)',
-    choices=('c', 'u'),
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument(
+    '-d',
+    '--decompress',
+    type=argparse.FileType('rb'),
+    help='Archivo a descomprimir',
 )
-parser.add_argument(
-    '-f',
-    '--file',
-    type=argparse.FileType('r',encoding='utf-8-sig'),
-    default=sys.stdin,
-    help='Archivo a comprimir o descomprimir',
+group.add_argument(
+    '-c',
+    '--compress',
+    type=argparse.FileType('r',encoding='utf-8-sig'),    
+    help='Archivo a comprimir ',
+    required=False,
 )
 args = parser.parse_args()
 
+
+
+
+
 #variables
-dictionary = {} 
-text = ""
-toCompare = ""
-toStoreInteger = 257
-dq = deque()
-binaryFile = bitarray()
+dictionary = {}
 
 
-#algorithm
-
-#Preload ascii to dictionary
-for i in range(256):
-  bitString = bin(i)[2:]
-  dictionary[chr(i)] = bitarray(bitString) 
 
 #compress algorithm
-if args.action == "c":
-  text = args.file.read()
+if args.compress:
+  print("comprimiendo")
+  #variables
+  text = ""
+  toCompare = ""
+  toStoreInteger = 257
+  dq = deque()
+  binaryFile = bitarray()
+  lenghtBinary = 0
+
+
+  #Preload ascii to dictionary
+  for i in range(256):
+    bitString = bin(i)[2:]
+    dictionary[chr(i)] = bitarray(bitString) 
+
+  #algorithm
+  text = args.compress.read()
+  print( text.isprintable() )
+  print( type(text) )
   for letter in text:
-    
     toCompare += letter
+    #print(str(toCompare in dictionary)+":"+toCompare+":"+toCompare[:-1]) 
     if toCompare in dictionary:
-      inDictionary=dictionary[toCompare]
       continue
     else:
+      
       bitString = bin(toStoreInteger)[2:]
-      dictionary[toCompare] = bitarray(bitString)     
+      dictionary[toCompare] = bitarray(bitString)    
+      inDictionary=dictionary[toCompare[:-1] ]
       
       dq.append(inDictionary)
+      #print(inDictionary)
       toCompare = toCompare[-1]
       toStoreInteger+=1
 
   maxBinLenght = len( bin(toStoreInteger) ) - 2 #longitud del ultimo binario almacenado
+  
+  lenghtBinary = bin(maxBinLenght)[2:]
+  lengtFill = 8 - len(lenghtBinary)
+  aleatoryToFill =  bitarray(lengtFill)
+  aleatoryToFill.setall(0)
+  binaryLenghFile = aleatoryToFill + bitarray(lenghtBinary)
+  binaryFile += binaryLenghFile
 
   for value in dq: 
     #print(value)  
     sizeToFill = maxBinLenght - value.length() 
+    #print(maxBinLenght,value.length() )
     zeroFill = bitarray(sizeToFill)
     zeroFill.setall(0)
     value = zeroFill + value 
                    
     binaryFile += value
 
+  # for i in dictionary:
+  #   print(i,dictionary[i])
+  
+  
+
   fileBin = open('out.bits','wb')
   binaryFile.tofile(fileBin)
   fileBin.close()
 
 #uncompress algoritm
-if args.action == "u":
-  print("descomprimiendo")
+if args.decompress:
+  #variables
+  binaryFile = bitarray()
+  binaryFile.fromfile(args.decompress)
+  lenght = {'binary':bitarray(),'integer':0}
+  code = {'binary':bitarray(),'integer':0,'cadena':''}
+
+  lenghtBynary = binaryFile[0:8]
+  lengtInt = ord( lenghtBynary.tostring() )
+  lenghtBynary = 0
+  dq = deque()
+  binaryCompare = bitarray()
+  intCompare = 0
+
+  # #Preload ascii to dictionary
+  # for i in range(256):
+  #   bitString = bin(i)[2:]
+  #   dictionary[bitString] =  chr(i)
+
+  #Preload ascii to dictionary int,character
+  for i in range(256):
+    dictionary[i] =  chr(i)
+
+  
+  
+  for i in range(8, len(binaryFile) ,lengtInt):
+    binary = binaryFile[i:i+lengtInt]
+    binaryCompare += binary
+    intCompare = int(binary.to01(),2)
+    #print(binary)
+    print( intCompare   )
+    if intCompare in dictionary:
+      continue:
+    else:
+      intCompare = int(binaryCompare.to01(),2)
+      dictionary[intCompare] = 
+    
+  # for i in dictionary:
+  #   print(i,dictionary[i])
+  # print()
